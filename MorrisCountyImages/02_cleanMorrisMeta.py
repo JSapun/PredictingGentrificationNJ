3575f27491b21902cf372e9dab86dea6d4b2f4fb
JSapun <sapun.justin@gmail.com>
2024-04-30 16:58:21 -0400

    5th Batch

---
+++ MorrisCountyImages/02_cleanMorrisMeta.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+from tqdm import tqdm
+
+
+# Check metadata for all addresses and print status
+
+
+def validate(df):
+    meta_base = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
+    api_key = open("../MapsAPI_Key.txt", "r").read()
+    morris_df = df.copy().sample(300)  # 100 samples per town
+
+    add_df = morris_df["ADDRESS"]
+    town_df = morris_df["POST_COMM"]
+    df_meta_response = [requests.get(meta_base, params={'key': api_key, 'location': x}) for x in add_df]
+    # 30000 entries = 40 mins
+
+    meta_resp_decoded = [x.json() for x in df_meta_response]
+    meta_resp_status = [x.get('status') for x in meta_resp_decoded]
+    meta_resp_id = [x.get('pano_id') for x in meta_resp_decoded]
+    meta_resp_lat = [x.get('location', {'lat': None})['lat'] for x in meta_resp_decoded]
+    meta_resp_lng = [x.get('location', {'lng': None})['lng'] for x in meta_resp_decoded]
+
+    meta_df = pd.DataFrame({'pano_id': meta_resp_id, 'status': meta_resp_status, 'town': town_df, 'lat': meta_resp_lat,
+                            'lng': meta_resp_lng, 'address': add_df})
+    cleaned_meta_df = meta_df[meta_df["pano_id"].notnull()].reset_index(drop=True).drop_duplicates()
+    return cleaned_meta_df
+
+
+def iterate_towns(df):
+    comb = pd.DataFrame()
+    for town in tqdm(towns):
+        ret = validate(df[df["POST_COMM"] == town])
+        comb = pd.concat([comb, ret], ignore_index=True)
+    return comb
+
+
+if __name__ == '__main__':
+    morris = pd.read_csv("./csv/morris_address.csv")
+    towns = list(pd.read_csv("./csv/morris_towns.csv").towns)
+
+    cleaned_morris_df = iterate_towns(morris)
+    cleaned_morris_df.to_csv("./csv/cleaned_morris_address.csv", index=False)
---
+++ MorrisCountyImages/03_getMoreImages.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+from streetview import search_panoramas, get_streetview
+from tqdm import tqdm
+
+
+# Get image pairs, a sample of 20 per each town in Morris County
+def save_image_pair(lat, lon, dir, sample):
+    panos = search_panoramas(lat=lat, lon=lon)
+
+    cleaned_panos = [panos[i] for i, x in enumerate([x.date for x in panos], 0) if
+                     x is not None]  # Only get images with clean dates
+    pano_dates = [x.date for x in cleaned_panos]
+    pano_ids = [x.pano_id for x in cleaned_panos]
+
+    # Check we have at least 2 panos
+    if len(pano_dates) < 2:
+        return False, []
+
+    # We want the first and last dates
+    pano_dates_subset = [pano_dates[0], pano_dates[-1]]
+    pano_ids_subset = [pano_ids[0], pano_ids[-1]]
+
+    # print(pano_dates_subset)
+    # print(pano_ids_subset)
+
+    # meta = [get_panorama_meta(pano_id=x, api_key=api_key) for x in pano_ids_subset]
+
+    # print("\n" + str([x.status for x in meta]))
+
+    images = [get_streetview(pano_id=x, api_key=api_key) for x in pano_ids_subset]  # actually gets image data
+
+    images[0].save(dir + "png/" + sample + "_old.png", "png")
+    images[1].save(dir + "png/" + sample + "_new.png", "png")
+
+    images[0].save(dir + "jpg/" + sample + "_old.jpg", "jpeg")
+    images[1].save(dir + "jpg/" + sample + "_new.jpg", "jpeg")
+
+    return True, pano_dates_subset
+
+
+def get_sample_size(df, towns, N, dir):
+    save_df = pd.DataFrame(columns=['imgs', 'pano_id', 'town', 'lat', 'long', 'address', 'dates'])
+    running_sum = 0
+    running_img_index = 0
+
+    for town in tqdm(towns):  # iterate through towns until desired amount received
+        shuffled_df = df[df.town == town].sample(frac=1).reset_index(drop=True)  # Sort and shuffle
+        check = False  # Some samples will fail
+        img_index = 0
+        i = 0
+        while i < N:  # Stop once sample is reached for each town
+            one_sample = pd.DataFrame()
+            dates = []
+            while not check:
+                if img_index >= len(shuffled_df):  # Huge problem
+                    print("\nSample Size too large, too many errors from Streetview!")
+                    print(town)
+                    print(img_index)
+                    print(i)
+                    exit()
+                one_sample = shuffled_df.iloc[img_index]
+                check, dates = save_image_pair(one_sample.lat.item(), one_sample.lng.item(), dir, str(running_img_index))
+                img_index += 1
+            row = [running_img_index, one_sample.pano_id, one_sample.town, one_sample.lat.item(), one_sample.lng.item(),
+                   one_sample.address, dates]
+            save_df.loc[running_img_index] = np.asarray(row, dtype="object")
+            running_img_index += 1
+            check = False
+            i += 1
+        running_sum += img_index
+
+    print("Iterated through: " + str(running_sum) + " images")
+    save_df.to_csv("./csv/randomImagePairInfo.csv", index=False)  # Save image information
+
+
+if __name__ == '__main__':
+    api_key = open("../MapsAPI_Key.txt", "r").read()
+    df = pd.read_csv("./csv/cleaned_morris_address.csv")
+    towns = list(pd.read_csv("./csv/morris_towns.csv").towns)
+    get_sample_size(df, towns, 20, "./streetviewImg/")  # 40 mins / 100, needed 106 for 39
+    print("Done")
